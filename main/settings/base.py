8f0c6f71187e09ef87c12f104cbc60244d53cbd8
Oznur Celebi <o.ozgumus17@gmail.com>
2022-03-10 14:43:27 +0100

    serializer added

--- main/settings/base.py
+++ main/settings/base.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    'rest_framework',
     'rest_framework.authtoken',
     "django.contrib.contenttypes",
     "django.contrib.sessions",
